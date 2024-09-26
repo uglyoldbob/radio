af3845959b0f70a1832b2a2cdc008974d228d7c0
Thomas Epperson <thomas.epperson@gmail.com>
2024-09-26 06:57:31 -0500

    Start properly decoding video. Doesn't compile yet.

--- src/video.rs
+++ src/video.rs
@@ -18,6 +18,7 @@ use v4l::FourCC;
 
 pub struct Video {
     image: Arc<Mutex<Vec<u8>>>,
+    texture: Option<egui::TextureHandle>,
 }
 
 impl Video {
@@ -26,28 +27,28 @@ impl Video {
         let i2 = image.clone();
         std::thread::spawn(move || {
             let mut dev = Device::new(0).expect("Failed to open video device");
+
             println!("Video caps: {:?}", dev.query_caps());
             println!("Video controls: {:?}", dev.query_controls());
-            println!("Video framesizes: {:?}", dev.enum_framesizes(FourCC::new(b"YUYV")));
+            println!("Video formats: {:?}", dev.enum_formats());
+            println!("Video framesizes YUYV: {:?}", dev.enum_framesizes(FourCC::new(b"YUYV")));
             let mut stream = MmapStream::with_buffers(&mut dev, Type::VideoCapture, 4).expect("Failed to create video buffer stream");
             loop {
                 let (buf, meta) = stream.next().unwrap();
                 let mut buf_rgb = vec![0; buf.len()];
+                println!("Image size {}", buf.len());
                 if let Ok(mut i) = i2.lock() {
                     *i = vec![0; buf.len()];
                     let a: &mut [u8] = &mut i;
-                    buf.iter().copied().pixels::<ffimage_yuv::yuv::Yuv<u8>>().colorconvert::<ffimage::color::Rgb<u8>>().bytes().write(a);
+                    let mut b = buf.iter().copied().pixels::<ffimage_yuv::yuv422::Yuv422<u8, 0,2,1,3>>();
+                    let c = b.colorconvert::<ffimage::color::Rgb<u8>>();
+                    c.write(a);
                 }
-                println!(
-                    "Video buffer size: {}, seq: {}, timestamp: {}",
-                   buf.len(),
-                   meta.sequence,
-                   meta.timestamp
-               );
             }
         });
         Self {
             image,
+            texture: None,
         }
     }
 }
@@ -62,11 +63,32 @@ impl SubwindowTrait for Video {
         egui::CentralPanel::default().show(ctx, |ui| {
             ui.label("This is the video page");
             if let Ok(i) = self.image.lock() {
-                let is = egui::ImageSource::Bytes {
-                    uri: "video0".into(),
-                    bytes: i.clone().into(),
-                };
-                ui.image(is);
+                if i.len() != 0 {
+                    let image = egui::ColorImage {
+                        size: [320 as usize, 240 as usize],
+                        pixels: i.chunks_exact(2).map(|i| {
+                            let j = (i[1] as u16) | (i[0] as u16) << 8;
+                            egui::Color32::from_rgb((j>>11) as u8, ((j>>5) & 0x3f) as u8, (j & 0x1F) as u8)
+                    }).collect(),
+                    };
+                    if let None = self.texture {
+                        self.texture =
+                            Some(ctx.load_texture("camera0", image, egui::TextureOptions::LINEAR));
+                    } else if let Some(t) = &mut self.texture {
+                        t.set_partial([0, 0], image, egui::TextureOptions::LINEAR);
+                    }
+                }
+            }
+            if let Some(t) = &self.texture {
+                let size = ui.available_size();
+                let zoom = (size.x / 320.0).min(size.y / 240.0);
+                let r = ui.add(egui::Image::from_texture(egui::load::SizedTexture {
+                    id: t.id(),
+                    size: egui::Vec2 {
+                        x: 320.0 * zoom,
+                        y: 240.0 * zoom,
+                    },
+                }));
             }
         });
         None
